175e7df9a21c3c513908695fdad7f8413f048571
Caique Vallim <caiquevallim@hotmail.com>
2018-03-09 00:44:32 -0300

    redux thunk implementation is going ok

--- src/components/component/_store/actions/ComponentCrudAction.js
+++ src/components/component/_store/actions/ComponentCrudAction.js
@@ -3,27 +3,52 @@ import {
     SAVE_DATA_REQUEST, SAVE_DATA_SUCCESS, SAVE_DATA_FAILURE
 } from './ComponentCrudAction.types'
 
+import ComponentServiceCrud from '../../services/Component.service.crud';
+import {componentListing} from "./ComponentScreenAction";
 
-export const fetchDataRequest = ()=>({
-    type: FETCH_DATA_REQUEST,
-});
-export const fetchDataFailure = (err)=>({
+const fetchData = ()=>( ComponentServiceCrud.findAll());
+const fetchDataRequest = ()=>({type: FETCH_DATA_REQUEST,});
+const fetchDataFailure = (err)=>({
     type: FETCH_DATA_FAILURE,
     payload:err.message
 });
-export const fetchDataSuccess = (data)=>({
+const fetchDataSuccess = (data)=>({
     type: FETCH_DATA_SUCCESS,
     payload:data
 });
-export const saveDataRequest = (data)=>({
+export function fetchDataAsync() {
+    return function (dispatch) {
+        dispatch(fetchDataRequest());
+        return fetchData().then(
+            data=>dispatch(fetchDataSuccess(data)),
+            error=>dispatch(fetchDataFailure(error))
+        )
+    }
+}
+
+const saveData = (data)=>(data && data.id && data.id !== '' ? ComponentServiceCrud.update(data):ComponentServiceCrud.create(data));
+const saveDataRequest = (data)=>({
     type:SAVE_DATA_REQUEST,
     payload:data
 });
-export const saveDataSuccess = (data)=>({
+const saveDataSuccess = (data)=>({
     type:SAVE_DATA_SUCCESS,
     payload:data
 });
-export const saveDataFailure = (err)=>({
+const saveDataFailure = (err)=>({
     type:SAVE_DATA_FAILURE,
     payload:err.message
 });
+export function saveDataAsync(data) {
+    return function (dispatch) {
+        dispatch(saveDataRequest(data));
+        return saveData(data).then(
+            data=> {
+                dispatch(saveDataSuccess(data));
+                dispatch(componentListing());
+                fetchDataAsync();
+            },
+            error=>dispatch(saveDataFailure(error))
+        )
+    }
+}
--- src/components/component/services/Component.service.js
+++ src/components/component/services/Component.service.js
@@ -1,46 +1,18 @@
 
 import store from '../../_store/store';
 import {
-    fetchDataFailure, fetchDataRequest, fetchDataSuccess, saveDataFailure,
-    saveDataRequest, saveDataSuccess
+    fetchDataAsync, saveDataAsync
 } from "../_store/actions/ComponentCrudAction";
-import ComponentServiceCrud from './Component.service.crud';
-import {componentListing} from "../_store/actions/ComponentScreenAction";
 
 class ComponentService{
     constructor(store){
         this._store = store;
     }
-    findAll(){
-        this._store.dispatch((dispatch)=>{
-            dispatch(fetchDataRequest());
-            ComponentServiceCrud.findAll()
-                .then(
-                    data=>dispatch(fetchDataSuccess(data.components?data.components:[]))
-                )
-                .catch(
-                    error=>dispatch(fetchDataFailure(error))
-                )
-        })
+    findAll() {
+        this._store.dispatch(fetchDataAsync());
     }
     create_update(objectToSave={}){
-        this._store.dispatch((dispatch)=>{
-            dispatch(saveDataRequest(objectToSave));
-
-            const {id} = objectToSave;
-            let promise = id && id !== '' ? ComponentServiceCrud.update(objectToSave):ComponentServiceCrud.create(objectToSave);
-            promise
-                .then(
-                    data=>{
-                        dispatch(saveDataSuccess(data));
-                        dispatch(componentListing());
-                        this.findAll();
-                    }
-                )
-                .catch(
-                    error=>dispatch(saveDataFailure(error))
-                )
-        })
+        this._store.dispatch(saveDataAsync(objectToSave));
     }
 
 
